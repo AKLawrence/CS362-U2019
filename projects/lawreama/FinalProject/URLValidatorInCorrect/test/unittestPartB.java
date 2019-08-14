ddaeb86a7f6972cb49b8bc758e3fe42bfa5b4bfa
AKLawrence <lawreama@oregonstate.edu>
2019-08-13 20:09:05 -0500

    unittestPartB.java can now read in the URLS.txt file

--- projects/lawreama/FinalProject/URLValidatorInCorrect/test/unittestPartB.java
+++ projects/lawreama/FinalProject/URLValidatorInCorrect/test/unittestPartB.java
@@ -1,4 +1,7 @@
 import junit.framework.TestCase;
+import java.io.BufferedReader; 
+import java.io.FileReader; 
+import java.io.IOException; 
 
 /**
  * Performs unit Tests for url validations.
@@ -6,8 +9,36 @@ import junit.framework.TestCase;
  * @version $Revision$
  */
  
-  public class UrlUnitTest extends TestCase {
-	
-	
-	
- }
+
+
+
+class BuffRead
+{ 
+    public static void main(String[] args) throws IOException  
+    { 
+        FileReader fr = new FileReader("URLS.txt"); 
+        BufferedReader br = new BufferedReader(fr); 
+        char c[]=new char[100]; 
+
+
+        if(br.ready()) {  
+            System.out.println(br.readLine()); 
+  
+            //read(char c[], int off, int len) 
+            br.read(c); 
+            for (int i = 0; i <100 ; i++) { 
+            	System.out.print(c[i]); 
+            } 
+            System.out.println(); 
+  
+            //reset() BufferedReader for the next line
+            br.reset(); 
+            
+        } 
+    } 
+}
+
+  /**public class UrlUnitTest extends TestCase {
+   *
+   *}
+   */
