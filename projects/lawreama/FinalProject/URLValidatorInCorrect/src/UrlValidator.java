0ca687c3158e9a5befe48945d44e2982163984b4
AKLawrence <lawreama@oregonstate.edu>
2019-08-13 22:00:09 -0500

    cleaned up change I previously made to UrlValidator.java

--- projects/lawreama/FinalProject/URLValidatorInCorrect/src/UrlValidator.java
+++ projects/lawreama/FinalProject/URLValidatorInCorrect/src/UrlValidator.java
@@ -14,8 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package src.UrlValidatorpkg;
-
 
 import java.io.Serializable;
 import java.net.URI;
